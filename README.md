d97d752f616d93c8be5ab6d752813cc16043dc35
Matthew Sklepowich <matthew@sklepowich.com>
2022-10-05 15:38:25 -0700

    added a number guessing game and a guessing game that checks via an array

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 This is a webpage that goes over my work history, school history, as well as some things that I enjoy. There is a quiz that users can participate in after opting in via the click of a button.
 
 ### Author - Matthew Sklepowich
+
+## Lab 03 Changes
+
+Added a "Top 10 Favorite Artists" to the webpage, as well as a "Guess the number I am thinking of" minigame. Both of these are incorportated into the previously-5-question quiz.
--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ function getName() {
   let tempName = prompt("Hi there! Please tell me your name!");
   if (tempName != "") {
     userName = tempName;
-    document.getElementById("welcome").innerHTML = "Welcome, " + tempName + "!";
+    document.getElementById("welcome").innerHTML = `Welcome, ${tempName}!`;
   } else {
     document.getElementById("welcome").innerHTML =
       "Welcome, friendly stranger!";
@@ -26,21 +26,22 @@ function gameStart() {
   if (ques1.toLowerCase() === "yes" || ques1.toLowerCase() === "y") {
     //console.log("Correct!");
     alert("You are correct!");
-    document.getElementById("q1").innerHTML =
-      "Question 1: You answered '" + ques1 + "', correct!";
+    document.getElementById(
+      "q1"
+    ).innerHTML = `Question 1: You answered '${ques1}', correct!`;
     score += 1;
   } else if (ques1.toLowerCase() === "no" || ques1.toLowerCase() === "n") {
     //console.log("Sorry, that is not correct.");
     alert("Sorry, that is not correct");
-    document.getElementById("q1").innerHTML =
-      "Question 1: You answered '" + ques1 + "', which is not correct :(";
+    document.getElementById(
+      "q1"
+    ).innerHTML = `Question 1: You answered '${ques1}', which is not correct :(`;
   } else {
     //console.log("You did not answer with 'Yes' or 'No'. Oh well, maybe next time.")
     alert("You did not answer with 'Yes' or 'No'. Oh well, maybe next time.");
-    document.getElementById("q1").innerHTML =
-      "Question 1: You answered '" +
-      ques1 +
-      "', which isn't even a real answer??";
+    document.getElementById(
+      "q1"
+    ).innerHTML = `Question 1: You answered '${ques1}', which isn't even a real answer??`;
   }
 
   // Question 2
@@ -48,21 +49,22 @@ function gameStart() {
   if (ques2.toLowerCase() === "no" || ques2.toLowerCase() === "n") {
     //console.log("Correct!");
     alert("You are correct!");
-    document.getElementById("q2").innerHTML =
-      "Question 2: You answered '" + ques2 + "', correct!";
+    document.getElementById(
+      "q2"
+    ).innerHTML = `Question 2: You answered '${ques2}', correct!`;
     score += 1;
   } else if (ques2.toLowerCase() === "yes" || ques2.toLowerCase() === "y") {
     //console.log("Sorry, that is not correct.");
     alert("Sorry, that is not correct");
-    document.getElementById("q2").innerHTML =
-      "Question 2: You answered '" + ques2 + "', which is not correct :(";
+    document.getElementById(
+      "q2"
+    ).innerHTML = `Question 2: You answered '${ques2}', which is not correct :(`;
   } else {
     //console.log("You did not answer with 'Yes' or 'No'. Oh well, maybe next time.")
     alert("You did not answer with 'Yes' or 'No'. Oh well, maybe next time.");
-    document.getElementById("q2").innerHTML =
-      "Question 2: You answered '" +
-      ques2 +
-      "', which isn't even a real answer??";
+    document.getElementById(
+      "q2"
+    ).innerHTML = `Question 2: You answered '${ques2}', which isn't even a real answer??`;
   }
 
   // Question 3
@@ -70,21 +72,22 @@ function gameStart() {
   if (ques3.toLowerCase() === "no" || ques3.toLowerCase() === "n") {
     //console.log("Correct!");
     alert("You are correct!");
-    document.getElementById("q3").innerHTML =
-      "Question 3: You answered '" + ques3 + "', correct!";
+    document.getElementById(
+      "q3"
+    ).innerHTML = `Question 3: You answered '${ques3}', correct!`;
     score += 1;
   } else if (ques3.toLowerCase() === "yes" || ques3.toLowerCase() === "y") {
     //console.log("Sorry, that is not correct.");
     alert("Sorry, that is not correct");
-    document.getElementById("q3").innerHTML =
-      "Question 3: You answered '" + ques3 + "', which is not correct :(";
+    document.getElementById(
+      "q3"
+    ).innerHTML = `Question 3: You answered '${ques3}', which is not correct :(`;
   } else {
     //console.log("You did not answer with 'Yes' or 'No'. Oh well, maybe next time.")
     alert("You did not answer with 'Yes' or 'No'. Oh well, maybe next time.");
-    document.getElementById("q3").innerHTML =
-      "Question 3: You answered '" +
-      ques3 +
-      "', which isn't even a real answer??";
+    document.getElementById(
+      "q3"
+    ).innerHTML = `Question 3: You answered '${ques3}', which isn't even a real answer??`;
   }
 
   // Question 4
@@ -94,21 +97,22 @@ function gameStart() {
   if (ques4.toLowerCase() === "yes" || ques4.toLowerCase() === "y") {
     //console.log("Correct!");
     alert("You are correct!");
-    document.getElementById("q4").innerHTML =
-      "Question 4: You answered '" + ques4 + "', correct!";
+    document.getElementById(
+      "q4"
+    ).innerHTML = `Question 4: You answered '${ques4}', correct!`;
     score += 1;
   } else if (ques4.toLowerCase() === "no" || ques4.toLowerCase() === "n") {
     //console.log("Sorry, that is not correct.");
     alert("Sorry, that is not correct");
-    document.getElementById("q4").innerHTML =
-      "Question 4: You answered '" + ques4 + "', which is not correct :(";
+    document.getElementById(
+      "q4"
+    ).innerHTML = `Question 4: You answered '${ques4}', which is not correct :(`;
   } else {
     //console.log("You did not answer with 'Yes' or 'No'. Oh well, maybe next time.")
     alert("You did not answer with 'Yes' or 'No'. Oh well, maybe next time.");
-    document.getElementById("q4").innerHTML =
-      "Question 4: You answered '" +
-      ques4 +
-      "', which isn't even a real answer??";
+    document.getElementById(
+      "q4"
+    ).innerHTML = `Question 4: You answered '${ques4}', which isn't even a real answer??`;
   }
 
   // Question 5
@@ -116,24 +120,130 @@ function gameStart() {
   if (ques5.toLowerCase() === "yes" || ques5.toLowerCase() === "y") {
     //console.log("Correct!");
     alert("You are correct!");
-    document.getElementById("q5").innerHTML =
-      "Question 5: You answered '" + ques5 + "', correct!";
+    document.getElementById(
+      "q5"
+    ).innerHTML = `Question 5: You answered '${ques5}', correct!`;
     score += 1;
   } else if (ques5.toLowerCase() === "no" || "n") {
     //console.log("Sorry, that is not correct.");
     alert("Sorry, that is not correct");
-    document.getElementById("q5").innerHTML =
-      "Question 5: You answered '" + ques5 + "', which is not correct :(";
+    document.getElementById(
+      "q5"
+    ).innerHTML = `Question 5: You answered '${ques5}', which is not correct :(`;
   } else {
     //console.log("You did not answer with 'Yes' or 'No'. Oh well, maybe next time.")
     alert("You did not answer with 'Yes' or 'No'. Oh well, maybe next time.");
-    document.getElementById("q5").innerHTML =
-      "Question 5: You answered '" +
-      ques5 +
-      "', which isn't even a real answer??";
+    document.getElementById(
+      "q5"
+    ).innerHTML = `Question 5: You answered '${ques5}', which isn't even a real answer??`;
   }
 
-  alert("Thanks for playing, " + userName + "! You got " + score + "/5!");
-  document.getElementById("score").innerHTML =
-    "Alright, you got " + score + "/5! Thanks for playing " + userName + "!";
+  alert("Okay, we're no longer answering with 'Yes' or 'No' here.");
+
+  // Question 6
+  let attempts = 4;
+  let numGuess = 24;
+  // Takes the User input and converts it to a number
+  let ques6 = parseInt(prompt("What number am I thinking of?"));
+  // If the answer is wrong and there are attempts, it gets chucked into this while loop
+  while (ques6 !== numGuess && attempts > 1) {
+    if (ques6 < numGuess) {
+      //Guess was too small
+      attempts -= 1;
+      //console.log(`You are too low. You have ${attempts} attempts left.`);
+      //console.log(attempts);
+      alert(
+        `You're too low. Think higher! You have ${attempts} attempts left.`
+      );
+    } else if (ques6 > numGuess) {
+      // Guess was too big
+      attempts -= 1;
+      //console.log(`You are too high. You have ${attempts} attempts left.`);
+      //console.log(attempts);
+      alert(
+        `You're too high. Think lower! You have ${attempts} attempts left.`
+      );
+    } else {
+      // Guess was not a num
+      attempts -= 1;
+      //console.log(`This was not a number. You have ${attempts} attempts left.`);
+      //console.log(attempts);
+      alert(
+        `That was not a number. Please enter a number. You have ${attempts} attempts left.`
+      );
+    }
+    ques6 = parseInt(prompt("What number am I thinking of?"));
+  }
+  // If input is right or there are no attempts left, skips while loop and comes here.
+  if (ques6 === numGuess) {
+    // User gets it right
+    score += 1;
+    alert("Correct! That's the number I was thinking of!");
+    document.getElementById(
+      "q6"
+    ).innerHTML = `Question 6: You answered '${ques6}', correct!`;
+  } else {
+    // User gets it wrong
+    alert(
+      `Sorry, you're out of guesses. The number I was thinking of was ${numGuess}.`
+    );
+    document.getElementById(
+      "q6"
+    ).innerHTML = `Question 6: You answered '${ques6}', which is not correct :(`;
+  }
+
+  // Question 7
+  let musicAttempts = 6;
+  let isFav = false;
+  let favBands = [
+    "Rush",
+    "Electric Light Orchestra",
+    "Pendulum",
+    "Genesis",
+    "TWRP",
+    "Casiopea",
+    "Styx",
+    "Breaking Benjamin",
+    "Starcadian",
+    "Gemini Syndrome",
+  ];
+  //while loop keeps going until user is out of attempts or they guess the right answer
+  while (musicAttempts > 0 && isFav === false) {
+    let ques7 = prompt("Who is one of my favorite artists?");
+    musicAttempts -= 1;
+    //checks each index in the favBands array
+    for (let i = 0; i < favBands.length; i++) {
+      if (ques7.toLowerCase() === favBands[i].toLowerCase()) {
+        alert(`That's correct! ${ques7} is one of my favorite artists!`);
+        score += 1;
+        //console.log(`Correct! ${ques7} is one of my fav bands`);
+        document.getElementById(
+          "q7"
+        ).innerHTML = `Question 7: You answered '${ques7}', which is one of my favorite artists!`;
+        isFav = true;
+        /*breaks us out of the for loop once we get the correct answer.
+        no reason to check the other indexes of the array.*/
+        break;
+      }
+    }
+    /*once for loop is finished, if input was not in array, check
+    that isFav is still false and we still have attempts*/
+    if (isFav === false && musicAttempts < 6) {
+      alert(
+        `That's not right. Remember the top 10 list... You have ${musicAttempts} attempts left.`
+      );
+    }
+    /*when user runs out of attempts, they get this final prompt*/
+    if (musicAttempts === 0) {
+      alert("Uh oh! You have run out of attempts.");
+      document.getElementById(
+        "q7"
+      ).innerHTML = `Question 7: You unfortunately did not guess one of my favorite bands.`;
+    }
+  }
+
+  alert(`Thanks for playing, ${userName}! You got ${score}/7!`);
+  document.getElementById(
+    "score"
+  ).innerHTML = `Alright, you got ${score}/7! Thanks for playing, ${userName}!`;
 }
